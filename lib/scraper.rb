d196cee798ac224dcf8855390268adad24824556
jose luis <cortezluisjose@gmail.com>
2019-09-22 00:07:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(html) = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html) = open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN")
 
